6d10528b1b77a5f9bbe48a5fc7b5dd8ecb838a53
Kai Duebbert <kai@mapsunited.com>
2019-12-18 09:29:09 +0000

    Fix forms test

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -6,46 +6,44 @@ from dbstorage.models import DBFile
 
 
 class DBFileFormTests(TestCase):
-
     def test_save(self):
-        form = DBFileForm(data={'name': 'hello-world.txt'}, files={
-            'file': SimpleUploadedFile('test.file', b'Hello World!'),
-        })
+        form = DBFileForm(
+            data={"name": "hello-world.txt"},
+            files={"file": SimpleUploadedFile("test.file", b"Hello World!"),},
+        )
         self.assertTrue(form.is_valid(), form.errors)
 
         db_file = form.save()
         self.assertTrue(db_file.pk)
         self.assertEqual(db_file.size, 12)
-        self.assertEqual(db_file.content, b'Hello World!')
-        self.assertEqual(db_file.name, 'hello-world.txt')
+        self.assertEqual(db_file.content, b"Hello World!")
+        self.assertEqual(db_file.name, "hello-world.txt")
 
     def test_save_with_instance(self):
-        content = b'Hello World!'
-        name = 'my-files/hello-world.txt'
+        content = b"Hello World!"
+        name = "my-files/hello-world.txt"
 
         db_file = DBFile.objects.create(content=content, name=name)
-        form = DBFileForm(instance=db_file, data={
-            'name': 'my-files/renamed.txt',
-        })
+        form = DBFileForm(instance=db_file, data={"name": "my-files/renamed.txt",})
         self.assertTrue(form.is_valid())
 
         new_db_file = form.save()
         self.assertEqual(db_file.pk, new_db_file.pk)
         self.assertEqual(db_file.content, new_db_file.content)
-        self.assertEqual('my-files/renamed.txt', new_db_file.name)
+        self.assertEqual("my-files/renamed.txt", new_db_file.name)
 
     def test_db_file_widget(self):
         widget = DBFileWidget()
         widget.instance = None
-        html = widget.render('file', '', )
-        self.assertEqual(html, '<input type="file" name="file" />')
+        html = widget.render("file", "",)
+        self.assertEqual(html, '<input type="file" name="file">')
 
     def test_db_file_widget_with_instance(self):
-        content = b'Hello World!'
-        name = 'my-files/hello-world.txt'
+        content = b"Hello World!"
+        name = "my-files/hello-world.txt"
         db_file = DBFile.objects.create(content=content, name=name)
 
         widget = DBFileWidget()
         widget.instance = db_file
-        html = widget.render('file', '', )
-        self.assertTrue('<table>' in html)
+        html = widget.render("file", "",)
+        self.assertTrue("<table>" in html)
